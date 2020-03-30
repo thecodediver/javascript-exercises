e48daab63ad907fea28bcc4837e1b49d8e51cedc
James <info@thecodediver.com>
2020-03-30 16:37:17 -0400

    completed more projects

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(obj) {
+    return obj.reduce((oldest, eachPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(eachPerson.yearOfBirth, eachPerson.yearOfDeath);
+        return oldestAge < currentAge ? eachPerson : oldest; 
+    })
+}
 
+const getAge = function(birth, death) {
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 }
 
 module.exports = findTheOldest
--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    return obj.map(nextTitle => nextTitle.title);
 }
 
 module.exports = getTheTitles;
